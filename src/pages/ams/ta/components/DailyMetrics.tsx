1ff90e24f58e8d974c461796a5b23eaf414fc293
Bhuvan Thota <sai.bhuvan@warpdrivetech.in>
2025-06-03 11:33:40 +0530

    Update DailyMetrics.tsx

--- src/pages/ams/ta/components/DailyMetrics.tsx
+++ src/pages/ams/ta/components/DailyMetrics.tsx
@@ -116,7 +116,7 @@ export const DailyMetrics: React.FC = () => {
         <EnhancedStreakCard />
         <EnhancedTodaysTargetsCard />
         <EnhancedTimelyTrackCard />
-        <EnhancedHiringFunnelCard />
+        <HiringFunnelCard />
       </div>
     </div>
   );
